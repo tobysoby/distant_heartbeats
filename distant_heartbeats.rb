a5b7a78c423e8eae63fe7899616698048b7d040f
Tobias Doll <tobias.doll@gmail.com>
2015-07-10 18:15:23 +0200

    cleaned code

--- distant_heartbeats.rb
+++ distant_heartbeats.rb
@@ -10,18 +10,6 @@ res = Net::HTTP.get(uri)
 res_json = JSON.parse(res)
 puts res_json["streams"][0]["loopStreamId"]
 
-=begin
-uri = URI("http://loopstream01.apa.at/?channel=fm4&id=" + res_json["streams"][0]["loopStreamId"])
-resp = Net::HTTP.get(uri)
-open("./hb_" + (Date.today-1).strftime("%d.%m.%Y") + ".mp3", "wb") do |file|
-	file.write(resp.body)
-end
-puts "Done."
-=end
-
 download = open("http://loopstream01.apa.at/?channel=fm4&id=" + res_json["streams"][0]["loopStreamId"])
 IO.copy_stream(download, "./hb_" + (Date.today-1).strftime("%d.%m.%Y") + ".mp3")
 puts "Done."
-
-#File.write("res.txt", res)
-#File.write("res_body.txt", res.body)
